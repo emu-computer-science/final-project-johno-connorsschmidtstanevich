1769caa25f5f4fd4eb0533b4b47a7edfb35871ee
Emmet Stanevich <emmet@stanevich.com>
2020-03-26 11:54:08 -0400

    Improved turning responsiveness
    Added a multiplier to acceleration when the control stick is held in the direction opposite to your horizontal movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,10 +8,36 @@ public class Player : MonoBehaviour
     [Header("Set in Inspector")]
     public float acceleration;
     public float maxSpeed;
+    public float turnMult = 1.0f;
 
     [Header("Set Dynamically")] public float speed;
     
     private Rigidbody2D _rb;
+
+    private int getDirection()
+    {
+        if (_rb.velocity.x > 0) return 1;
+        else if (_rb.velocity.x < 0) return -1;
+        else return 0;
+    }
+
+    private bool isTurning()
+    {
+        switch (getDirection())
+        {
+            case 0:
+                return false;
+            break;
+            case 1:
+                return Input.GetAxis("Horizontal") < 0;
+            break;
+            case -1:
+                return Input.GetAxis("Horizontal") > 0;
+            break;
+            default:
+                return false;
+        }
+    }
     
     // Start is called before the first frame update
     void Start()
@@ -28,6 +54,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         float deltaX = Input.GetAxis("Horizontal");
+        if (isTurning()) deltaX *= Mathf.Max(turnMult, 1);
         Vector2 movement = new Vector2(deltaX, 0.0f);
         _rb.AddForce(movement * acceleration);
     }
