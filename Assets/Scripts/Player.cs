f94c0ce69283a2e108e11a23bc89559bf0a394a7
Emmet Stanevich <emmet@stanevich.com>
2020-03-26 11:53:03 -0400

    Implemented speed limit
    Hopefully, this should allow me to smoothly pull the player back to their intended max speed should they be moving too fast, such as if they overtake another player, or if someone in front of them changes lanes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -32,13 +32,13 @@ public class Player : MonoBehaviour
         _rb.AddForce(movement * acceleration);
     }
 
-    // private void FixedUpdate()
-    // {
-    //     speed = _rb.velocity.x;
-    //     if (Mathf.Abs(speed) > maxSpeed)
-    //     {
-    //         Vector2 targetSpeed = new Vector2(maxSpeed, 0.0f);
-    //         _rb.velocity = Vector2.Lerp(new Vector2(speed, 0.0f).normalized, targetSpeed.normalized, 0.05f);
-    //     }
-    // }
+    private void FixedUpdate()
+    {
+        speed = _rb.velocity.x;
+        if (Mathf.Abs(speed) > maxSpeed)
+        {
+            Vector2 targetSpeed = new Vector2(maxSpeed, 0.0f);
+            _rb.velocity = Vector2.Lerp(new Vector2(speed, 0.0f).normalized, targetSpeed.normalized, 0.05f);
+        }
+    }
 }
