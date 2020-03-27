0328dfcf7b2d37622434a526be5bd5faee8319f4
Emmet Stanevich <emmet@stanevich.com>
2020-03-27 18:07:16 -0400

    Added jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,10 +9,33 @@ public class Player : MonoBehaviour
     public float acceleration;
     public float maxSpeed;
     public float turnMult = 1.0f;
+    public float jumpForce;
 
     [Header("Set Dynamically")] public float speed;
     
     private Rigidbody2D _rb;
+
+    private Animator _animator;
+    private static readonly int Speed = Animator.StringToHash("Speed");
+    private Collider2D _collider;
+    private static readonly int Grounded = Animator.StringToHash("Grounded");
+
+    private bool _IsGrounded;
+    private bool IsGrounded
+    {
+        get
+        {
+            if (_IsGrounded)
+            {
+                _IsGrounded = false;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
 
     private int getDirection()
     {
@@ -35,7 +58,22 @@ public class Player : MonoBehaviour
                 return false;
         }
     }
-    
+
+    // private bool IsGrounded()
+    // {
+    //     return Physics.CheckBox(_collider.bounds.center,
+    //         new Vector2(_collider.bounds.center.x, _collider.bounds.min.y - 0.1f));
+    // }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        foreach (var contact in other.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+            if (Math.Abs(contact.point.y - _collider.bounds.min.y) < 0.01f) _IsGrounded = true;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -44,7 +82,9 @@ public class Player : MonoBehaviour
 
     private void Awake()
     {
+        _collider = GetComponent<Collider2D>();
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -54,6 +94,20 @@ public class Player : MonoBehaviour
         if (isTurning()) deltaX *= Mathf.Max(turnMult, 1);
         Vector2 movement = new Vector2(deltaX, 0.0f);
         _rb.AddForce(movement * (acceleration * Time.deltaTime));
+        _animator.SetFloat(Speed, Mathf.Abs(_rb.velocity.x));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (IsGrounded) _rb.AddForce(new Vector2(0.0f, jumpForce));
+            else if (_rb.velocity.y > 0) _rb.AddForce(Physics.gravity * (-0.5f * Time.deltaTime));
+        }
+        
+        _animator.SetBool(Grounded, _IsGrounded);
+        
+        // if (Mathf.Abs(_rb.velocity.x) >= 100)
+        // {
+        //     
+        // }
     }
 
     private void FixedUpdate()
@@ -65,4 +119,6 @@ public class Player : MonoBehaviour
             _rb.velocity = Vector2.Lerp(new Vector2(speed, 0.0f).normalized, targetSpeed.normalized, 0.05f);
         }
     }
+    
+    
 }
