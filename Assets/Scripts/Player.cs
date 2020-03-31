31fc12b47d87363dc0e48005db4629d689293229
Emmet Stanevich <emmet@stanevich.com>
2020-03-31 14:21:36 -0400

    Changed getter methods to properties
    Also changed jump action to be triggered by OnJump() function

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,14 +24,14 @@ public class Player : MonoBehaviour, Controls.IGameplayActions
 
     Controls _controls;
 
-    private bool _IsGrounded;
+    private bool _isGrounded;
     private bool IsGrounded
     {
         get
         {
-            if (_IsGrounded)
+            if (_isGrounded)
             {
-                _IsGrounded = false;
+                _isGrounded = false;
                 return true;
             }
             else
@@ -41,25 +41,30 @@ public class Player : MonoBehaviour, Controls.IGameplayActions
         }
     }
 
-    private int getDirection()
+    private int GetDirection
     {
-        if (_rb.velocity.x > 0) return 1;
-        else if (_rb.velocity.x < 0) return -1;
-        else return 0;
+        get{
+            if (_rb.velocity.x > 0) return 1;
+            else if (_rb.velocity.x < 0) return -1;
+            else return 0;}
+        
     }
 
-    private bool isTurning()
+    private bool IsTurning
     {
-        switch (getDirection())
+        get
         {
-            case 0:
-                return false;
-            case 1:
-                return Input.GetAxis("Horizontal") < 0;
-            case -1:
-                return Input.GetAxis("Horizontal") > 0;
-            default:
-                return false;
+            switch (GetDirection)
+            {
+                case 0:
+                    return false;
+                case 1:
+                    return Input.GetAxis("Horizontal") < 0;
+                case -1:
+                    return Input.GetAxis("Horizontal") > 0;
+                default:
+                    return false;
+            }
         }
     }
 
@@ -74,7 +79,7 @@ public class Player : MonoBehaviour, Controls.IGameplayActions
         foreach (var contact in other.contacts)
         {
             Debug.DrawRay(contact.point, contact.normal, Color.white);
-            if (Math.Abs(contact.point.y - _collider.bounds.min.y) < 0.01f) _IsGrounded = true;
+            if (Math.Abs(contact.point.y - _collider.bounds.min.y) < 0.01f) _isGrounded = true;
         }
     }
 
@@ -133,6 +138,7 @@ public class Player : MonoBehaviour, Controls.IGameplayActions
     public void OnJump(InputAction.CallbackContext context)
     {
         Debug.Log("Jump");
+        if (IsGrounded) _rb.AddForce(new Vector2(0.0f, jumpForce));
     }
 
     public void OnGrapple(InputAction.CallbackContext context)
@@ -145,7 +151,7 @@ public class Player : MonoBehaviour, Controls.IGameplayActions
     {
         // float deltaX = Input.GetAxis("Horizontal");
         float deltaX = _joyPosX;
-        if (isTurning()) deltaX *= Mathf.Max(turnMult, 1);
+        if (IsTurning) deltaX *= Mathf.Max(turnMult, 1);
         Vector2 movement = new Vector2(deltaX, 0.0f);
         _rb.AddForce(movement * (acceleration * Time.deltaTime));
         _animator.SetFloat(Speed, Mathf.Abs(_rb.velocity.x));
@@ -154,11 +160,10 @@ public class Player : MonoBehaviour, Controls.IGameplayActions
 
         if (Input.GetButtonDown("Jump"))
         {
-            if (IsGrounded) _rb.AddForce(new Vector2(0.0f, jumpForce));
-            else if (_rb.velocity.y > 0) _rb.AddForce(Physics.gravity * (-0.5f * Time.deltaTime));
+            if (_rb.velocity.y > 0) _rb.AddForce(Physics.gravity * (-0.5f * Time.deltaTime));
         }
         
-        _animator.SetBool(Grounded, _IsGrounded);
+        _animator.SetBool(Grounded, _isGrounded);
         
         // if (Mathf.Abs(_rb.velocity.x) >= 100)
         // {
