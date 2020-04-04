9cf8b2a42134ab54876ed4de68d7b437d356e3b2
Emmet Stanevich <emmet@stanevich.com>
2020-04-04 12:34:56 -0400

    Began compartmentalizing physics scripts
    It may be a good idea to start separating aspects of the scripts, so that we can tweak and replace parts a bit easier.

---
+++ Assets/Scripts/JumpPhysics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class JumpPhysics : MonoBehaviour
+{
+        [Header("Set in Inspector")]
+        public float fallMultiplier;
+        public float lowJumpMultiplier;
+        
+        private Rigidbody2D _rb;
+
+        private void Awake()
+        {
+                _rb = GetComponent<Rigidbody2D>();
+        }
+}
