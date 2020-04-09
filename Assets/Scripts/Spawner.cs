f3e889d5fd1303becd5caad201ecbe5edbb8ab20
Emmet Stanevich <emmet@stanevich.com>
2020-04-09 17:21:45 -0400

    Reimplemented multiplayer in terrain generation test room
    The spawner will place each player at a designated position, which can be set in the editor.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 
 public class Spawner : MonoBehaviour
 {
+    [Header("Set in Inspector")]
     public Color[] PlayerColors = {new Color(192, 0, 0), new Color(0, 0, 192), new Color(0, 192, 0), Color.yellow};
     public Vector2[] SpawnPoints = {new Vector2(0, 0), new Vector2(16, 16), new Vector2(0, 32), new Vector2(0, 48)};
 
