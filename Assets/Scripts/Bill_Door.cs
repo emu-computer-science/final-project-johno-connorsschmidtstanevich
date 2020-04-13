41ead9cac5e232d00c5b58a56b833430b17222bf
Emmet Stanevich <emmet@stanevich.com>
2020-04-13 00:51:09 -0400

    Locks player during death
    When you die, the game turns you invisible, disables your controls, and pauses your physics. After 3 seconds, it then puts you at your spawn point, frees your controls, resumes your physics, turns you visible, and resets your momentum to 0.

--- Assets/Scripts/Bill_Door.cs
+++ Assets/Scripts/Bill_Door.cs
@@ -33,17 +33,20 @@ public class Bill_Door : MonoBehaviour
     IEnumerator Respawn(PlayerInput player)
     {
         player.DeactivateInput();
+        player.GetComponent<Rigidbody2D>().simulated = false;
         foreach (var sprite in player.GetComponent<Player>().Sprites)
         {
             sprite.enabled = false;
         }
         Debug.Log($"Player {player.playerIndex + 1} has died!");
-        yield return new WaitForSecondsRealtime(0);
+        yield return new WaitForSecondsRealtime(3);
         player.ActivateInput();
         player.gameObject.transform.position = _spawner.SpawnPoints[player.playerIndex];
         foreach (var sprite in player.GetComponent<Player>().Sprites)
         {
             sprite.enabled = true;
         }
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        player.GetComponent<Rigidbody2D>().simulated = true;
     }
 }
