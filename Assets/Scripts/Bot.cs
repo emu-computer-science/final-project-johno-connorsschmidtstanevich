8942111175dc3235785e03893a83f82a136238d8
MyPalBaco <34905591+MyPalBaco@users.noreply.github.com>
2020-04-18 14:42:50 -0400

    Added Enemy
    Added enemy to the game that fires projectiles at the player

---
+++ Assets/Scripts/Bot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot : MonoBehaviour
+{
+    //players must not have this player
+    public GameObject[] players;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Only switch if no player on the same lane in front
+        bool inFront = false;
+        int closestPlayer;
+        
+        //Check if player is in front
+        for(int i = 0; i < players.Length; i++)
+        {
+            float x = players[i].transform.position.x;
+            float y = players[i].transform.position.y;
+
+            if (x > this.transform.position.x && y == this.transform.position.y)
+            {
+                inFront = true;
+                break;
+            }
+        }
+
+        //Change lanes
+        if(inFront == false)
+        {
+            //Find closest player by height
+            
+        }
+    }
+}
