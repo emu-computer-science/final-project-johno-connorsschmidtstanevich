87a91e42c92fd05a900b93e1eb3e14fa8e802266
Emmet Stanevich <emmet@stanevich.com>
2020-04-19 15:32:30 -0400

    Implemented background scrolling
    The clouds will now scroll as the camera moves, based on the camera's left border.

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Cloud : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Camera _camera;
+    private CameraController _cameraController;
+    private RectTransform _rectTransform;
+
+    private float WrapLength
     {
-        
+        get
+        {
+            return _rectTransform.rect.width/2;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private Vector2 WrapPosition
     {
-        
+        get
+        {
+            return new Vector2((CameraPosition % WrapLength), 0);
+        }
+    }
+
+    /**
+     * Returns the position of the left side of the camera viewport
+     */
+    private float CameraPosition
+    {
+        get
+        {
+            return _camera.transform.position.x-_cameraController.CameraHorizontalExtent;
+        }
+    }
+
+    public float cameraPos;
+
+    
+
+    private void Awake()
+    {
+        _camera = GetComponentInParent<PlayerInput>().camera;
+        _cameraController = _camera.GetComponent<CameraController>();
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void LateUpdate()
+    {
+        _rectTransform.anchoredPosition = -WrapPosition;
+        cameraPos = CameraPosition;
     }
 }
