6b9115b7d7dac8f8b61b8955869c121cc042e6e2
Emmet Stanevich <emmet@stanevich.com>
2020-04-19 19:09:44 -0400

    Improved background scrolling
    The scroll repeat interval is now tied to the sprite being used, including its scale.

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -3,54 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Cloud : MonoBehaviour
 {
-    Camera _camera;
+    private Camera _camera;
     private CameraController _cameraController;
     private RectTransform _rectTransform;
+    private Image _image;
 
-    private float WrapLength
-    {
-        get
-        {
-            return _rectTransform.rect.width/2;
-        }
-    }
+    private float WrapLength => HorizontalTileSize;
 
-    private Vector2 WrapPosition
+    private Vector2 WrapPosition => new Vector2(CameraPosition % WrapLength, 0);
+
+    private float HorizontalTileSize => _image.sprite.texture.width * _image.rectTransform.localScale.x;
+    
+    private int TilesNeeded => Mathf.CeilToInt(_cameraController.CameraHorizontalExtent * 2 / HorizontalTileSize);
+
+    private void Initialize()
     {
-        get
-        {
-            return new Vector2((CameraPosition % WrapLength), 0);
-        }
+        _rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, HorizontalTileSize * Mathf.Max(TilesNeeded, 4));
     }
 
     /**
      * Returns the position of the left side of the camera viewport
      */
-    private float CameraPosition
-    {
-        get
-        {
-            return _camera.transform.position.x-_cameraController.CameraHorizontalExtent;
-        }
-    }
-
-    public float cameraPos;
-
-    
+    private float CameraPosition => _camera.transform.position.x - _cameraController.CameraHorizontalExtent;
 
     private void Awake()
     {
         _camera = GetComponentInParent<PlayerInput>().camera;
         _cameraController = _camera.GetComponent<CameraController>();
         _rectTransform = GetComponent<RectTransform>();
+        _image = GetComponent<Image>();
+        Initialize();
     }
 
     private void LateUpdate()
     {
         _rectTransform.anchoredPosition = -WrapPosition;
-        cameraPos = CameraPosition;
     }
 }
