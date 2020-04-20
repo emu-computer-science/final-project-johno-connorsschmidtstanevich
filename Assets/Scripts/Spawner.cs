c2769d2e55c02c17a45b18e5a235e805125b690a
Emmet Stanevich <emmet@stanevich.com>
2020-04-19 20:09:45 -0400

    Refactored serialized fields

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class Spawner : MonoBehaviour
 {
-    [Header("Set in Inspector")]
-    public Color[] PlayerColors = {new Color(192, 0, 0), new Color(0, 0, 192), new Color(0, 192, 0), Color.yellow};
-    public Vector2[] SpawnPoints = {new Vector2(0, 0), new Vector2(16, 16), new Vector2(0, 32), new Vector2(0, 48)};
+    [FormerlySerializedAs("PlayerColors")] [Header("Set in Inspector")]
+    public Color[] playerColors = {new Color(192, 0, 0), new Color(0, 0, 192), new Color(0, 192, 0), Color.yellow};
+    [FormerlySerializedAs("SpawnPoints")] public Vector2[] spawnPoints = {new Vector2(0, 0), new Vector2(16, 16), new Vector2(0, 32), new Vector2(0, 48)};
 
     private void OnPlayerJoined(PlayerInput player)
     {
         Debug.Log($"Player {player.playerIndex + 1} has joined!");
-        player.transform.position = SpawnPoints[player.playerIndex];
-        player.gameObject.GetComponentsInChildren<SpriteRenderer>()[1].color = PlayerColors[player.playerIndex];
+        player.transform.position = spawnPoints[player.playerIndex];
+        player.gameObject.GetComponentsInChildren<SpriteRenderer>()[1].color = playerColors[player.playerIndex];
     }
 }
