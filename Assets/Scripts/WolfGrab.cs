75eb4c6c8a8ebcc14ea06d6c0ec05c40eff304c0
Emmet Stanevich <emmet@stanevich.com>
2020-04-20 02:08:30 -0400

    Added WolfGrab script
    This script will simply check at the end of the animation if there is someone within grab range.

---
+++ Assets/Scripts/WolfGrab.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WolfGrab : StateMachineBehaviour
+{
+    private PlayerInput _player;
+    private List<PlayerInput> OtherPlayers
+    {
+        get
+        {
+            var ph = new List<PlayerInput>(PlayerInput.all);
+            ph.Remove(_player);
+            return ph;
+        }
+    }
+    
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    // override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    // {
+    //     
+    // }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _player = animator.GetComponent<PlayerInput>();
+                foreach (var otherPlayer in OtherPlayers)
+                {
+                    if(animator.GetComponent<Collider2D>().IsTouching(otherPlayer.GetComponent<Player>().HurtBox))
+                    {
+                        animator.SetTrigger("Grab");
+                        break;
+                    }
+                }
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
