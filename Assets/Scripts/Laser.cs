c3ea0c9cfe8f4b3a1db8e43f3d8fad04a4362e21
Emmet Stanevich <emmet@stanevich.com>
2020-04-20 14:06:03 -0400

    Laser movement now handled by physics engine
    Also set its despawn to be handled by a coroutine.

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -1,20 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
     public int speed = 100;
-    public float timeLeft = 20;
+    public float timeLeft = 5;
+    private Rigidbody2D _rb;
 
-    void Update()
+    private void Awake()
     {
-        transform.position += -transform.right * Time.deltaTime * speed;
-
-        timeLeft -= Time.deltaTime;
-        if(timeLeft <= 0)
-            Destroy(this);
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.velocity = Vector2.left * speed;
+        StartCoroutine(LaserTimer(timeLeft));
     }
+
+    IEnumerator LaserTimer(float s)
+    {
+        yield return new WaitForSeconds(s);
+        Destroy(gameObject);
+    }
+
+    // void Update()
+    // {
+    //     transform.position += -transform.right * Time.deltaTime * speed;
+    //
+    //     timeLeft -= Time.deltaTime;
+    //     if(timeLeft <= 0)
+    //         Destroy(gameObject);
+    // }
+    
+    
 
 
 }
