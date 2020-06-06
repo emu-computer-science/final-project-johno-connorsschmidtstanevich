e94ffc6dfd00eb327332d0f68ba20b556f97134d
Emmet Stanevich <emmet@stanevich.com>
2020-06-06 18:54:48 -0400

    Added volume slider script
    The volume slider will now update a value in the player preferences, allowing it to be accessed between scenes and sessions, meaning that you won't have to set the volume every time you start the game. If you haven't set the volume previously, it defaults to 50%.

---
+++ Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class VolumeSlider : MonoBehaviour
+{
+    private Slider _slider;
+
+    private void Awake()
+    {
+        _slider = gameObject.GetComponent<Slider>();
+        _slider.value = Mathf.Clamp(0, PlayerPrefs.GetFloat("volume", 50), 100);
+    }
+
+    public void SetVolume(float newVolume)
+    {
+        PlayerPrefs.SetFloat("volume", Mathf.Clamp(0, newVolume, 100));
+    }
+}
