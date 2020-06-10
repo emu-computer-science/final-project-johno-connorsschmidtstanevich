ee0b8d161ac5fd81455f49798ff4bb63a111a4a0
Emmet Stanevich <emmet@stanevich.com>
2020-06-10 17:09:46 -0400

    Restructured volume menu
    Now has separate sliders for music, SFX (hitsounds, taunts, etc.) as well as a master slider that controls the overall volume. Volume settings will now be stored in PlayerPrefs, and retrieved whenever the options menu loads.

--- Assets/Scripts/Menus/VolumeSlider.cs
+++ Assets/Scripts/Menus/VolumeSlider.cs
@@ -1,19 +1,41 @@
 ﻿using System;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VolumeSlider : MonoBehaviour
 {
-    private Slider _slider;
+    [Header("Set in Inspector")]
+    public AudioMixer audioMixer;
+
+    private Slider[] _sliders;
 
     private void Awake()
     {
-        _slider = gameObject.GetComponent<Slider>();
-        _slider.value = Mathf.Clamp(0, PlayerPrefs.GetFloat("volume", 50), 100);
+        _sliders = GetComponentsInChildren<Slider>();
+        _sliders[0].value = PlayerPrefs.GetFloat("masterVolume", 0.75f);
+        _sliders[1].value = PlayerPrefs.GetFloat("musicVolume", 1f);
+        _sliders[2].value = PlayerPrefs.GetFloat("sfxVolume", 1f);
     }
 
-    public void SetVolume(float newVolume)
+    public void SetVolume(String target, float newVolume)
     {
-        PlayerPrefs.SetFloat("volume", Mathf.Clamp(0, newVolume, 100));
+        audioMixer.SetFloat(target, Mathf.Log10(newVolume) * 20);
+        PlayerPrefs.SetFloat(target, Mathf.Clamp(newVolume, 0.0001f, 1f));
+    }
+
+    public void SetMasterVolume(float newVolume)
+    {
+        SetVolume("masterVolume", newVolume);
+    }
+
+    public void SetMusicVolume(float newVolume)
+    {
+        SetVolume("musicVolume", newVolume);
+    }
+
+    public void SetSfxVolume(float newVolume)
+    {
+        SetVolume("sfxVolume", newVolume);
     }
 }
